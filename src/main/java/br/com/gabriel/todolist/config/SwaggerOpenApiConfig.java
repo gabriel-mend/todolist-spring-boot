7b38f485384f4fba0509ddfd9ddf543c2540b5b1
gabriel <gabriel.pereira@ceuma.br>
2023-10-17 19:09:33 -0300

    feat: adicionando swagger no projeto

---
+++ src/main/java/br/com/gabriel/todolist/config/SwaggerOpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.gabriel.todolist.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerOpenApiConfig {
+    @Bean
+    public
+    OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("RESTFul API with Java 18 and Spring Boot 3")
+                .version("v1")
+                .description("Some description about your API")
+                .termsOfService("URL")
+                .license(new License().name("Apache 2.0").url("URL")));
+    }
+}
